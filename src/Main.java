c6d8f1d634ffab01721d85045b4c3a0bf8bfcef4
ESSADQUI <soumia.essadqui-etu@etu.univh2c.ma>
2026-03-01 21:18:18 +0000

    Suppression complète de tous les dossiers et fichiers

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import metier.Compte;
-public class Main {
-    public static void main(String[] args) {
-        Compte c1= new Compte(300);
-        c1.verser((float) 10.90F);
-        System.out.println(c1.toString());
-        }
-    }
